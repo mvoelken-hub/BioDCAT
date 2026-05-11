be640b1ea460b950c52bf6f9374507ac515f6446
mvoelken-hub <marc.voelkenrath@tu-dortmund.de>
2026-05-11 09:30:59 +0200

    initial schema setup - main branch

--- src/strendcat_biocatalysis/datamodel/strendcat_biocatalysis.py
+++ src/strendcat_biocatalysis/datamodel/strendcat_biocatalysis.py
@@ -1,5 +1,5 @@
 # Auto generated from strendcat_biocatalysis.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-05-08T11:44:04
+# Generation date: 2026-05-11T09:30:26
 # Schema: StrenDCAT-Biocatalysis
 #
 # id: https://w3id.org/mvoelken-hub/StrenDCAT-Biocatalysis
